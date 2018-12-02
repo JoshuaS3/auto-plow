6567e2625d5cf6567977d6376796cc0e6e2bbc78
joshuas3 <joshstockin@gmail.com>
2018-12-02 04:56:20 +0000

    Fix ON and OFF definitions
    A bit of an embarrassing error: ON and OFF were previously defined as 0 and 1, respectively, not the other way around like they should properly be. This is now fixed.

--- src/ard1/ard1.h
+++ src/ard1/ard1.h
@@ -1,10 +1,10 @@
 #ifndef __ARD1_H__
 #define __ARD1_H__ 1
 
-#define PIN 	  13
+#define PIN 13
 #define BAUD_RATE 9600
-#define ON 		  0
-#define OFF 	  1
+#define ON 1
+#define OFF 0
 
 void (* reset) (void) = 0;
 
