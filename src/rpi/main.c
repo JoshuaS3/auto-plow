93a17bd834e72425d9136abd4d301e0aa393b263
joshuas3 <joshstockin@gmail.com>
2018-12-02 05:34:00 +0000

    Set Arduino configuration, reset before reading
    Implements the following command to make sure the Arduino's configuration is okay:
    stty -F /dev/ttyACM0 9600 raw -clocal -echo
    Also pings the device with the "Restart;" command to reset it.

--- src/rpi/main.c
+++ src/rpi/main.c
@@ -1,13 +1,19 @@
 #include "stdio.h"
+#include "stdlib.h"
 
 int main() {
   printf("AutoPlow starting...\n\n");
+
+  printf("Attempting to set proper device configuration on ARD1\n");
+  system("stty -F /dev/ttyACM0 9600 raw -clocal -echo");
 
   printf("Attempting to begin serial data read on ARD1 (Motor Controller) at /dev/ttyACM0\n");
   FILE * ttyACM0;
   ttyACM0 = fopen("/dev/ttyACM0", "r");
   if (ttyACM0) {
     printf("Successfully opened serial stream\n\n");
+    printf("Attempting device reset\n");
+    fputs("Restart;", ttyACM0);
     printf("Dumping contents:\n");
     char buffer[4096];
     while (fgets(buffer, sizeof(buffer), ttyACM0) != NULL) {
