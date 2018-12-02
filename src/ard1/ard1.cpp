90c3edcb09be4477997511b820d6e3d96c33b4ea
joshuas3 <joshstockin@gmail.com>
2018-12-02 03:50:24 +0000

    Update ard1.cpp

--- src/ard1/ard1.cpp
+++ src/ard1/ard1.cpp
@@ -8,15 +8,23 @@ void setup() {
   while (!Serial)
     ;
   Serial.println("ARD1: Motor Controller Active");
+  Serial.println("ARD1: Hello, World!");
 }
+
+void (* resetFunc) (void) = 0;
 
 
 // Repeatedly
 void loop() {
   if (Serial.available() > 0) {
     Serial.print("ARD1: Received information: ");
-    Serial.println(Serial.readStringUntil(';'));
+    String message = Serial.readStringUntil(';');
+    Serial.println(message);
     Serial.flush();
+    if (message == String("Restart")) {
+      Serial.println("ARD1: Restarting");
+      Serial.flush();
+      resetFunc();
+    }
   }
-  delay(30);
 }
