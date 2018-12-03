d399c5d8b35078688ce34d5fb97aefae2c673ab3
joshuas3 <joshstockin@gmail.com>
2018-12-03 00:25:14 +0000

    Create `serial' function
    Enables the Serial data stream in better, more organized way. The serial.cpp file will soon be filled with other related functions regarding output and input.

---
+++ src/ard1/serial.cpp
@@ -0,0 +1,10 @@
+#include "Arduino.h"
+#include "ard1.h"
+#include "serial.h"
+
+void serial(void) {
+  if (!Serial) return;
+  Serial.begin(BAUD_RATE);
+  while (!Serial)
+    ;
+}
---
+++ src/ard1/serial.h
@@ -0,0 +1,6 @@
+#ifndef __SERIAL_H__
+#define __SERIAL_H__ 1
+
+void serial(void);
+
+#endif
