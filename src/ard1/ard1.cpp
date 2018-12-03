39f7c59036224c30224c5c56614596ae9f56f4a0
joshuas3 <joshstockin@gmail.com>
2018-12-03 00:26:31 +0000

    Update ard1.cpp
    Utilizes the new `serial.cpp' function. Also reorganizes some `setup' actions, particularly the enabling of the active LED pin.

--- src/ard1/ard1.cpp
+++ src/ard1/ard1.cpp
@@ -1,16 +1,21 @@
 #include "Arduino.h"
 #include "ard1.h"
+#include "serial.h"
 #include "reset.h"
 
 // One time
 void setup() {
   digitalWrite(PIN_RESET, HIGH);
-  pinMode(PIN, OUTPUT);
-  digitalWrite(PIN, ON);
-  Serial.begin(BAUD_RATE);
-  while (!Serial)
-    ;
+  serial();
   Serial.println("ARD1: Motor Controller Active");
+  Serial.print("ARD1: Setting PIN ");
+  Serial.print(PIN);
+  Serial.println("'s mode to OUTPUT");
+  pinMode(PIN, OUTPUT);
+  Serial.print("ARD1: Turning PIN ");
+  Serial.print(PIN);
+  Serial.println(" ON");
+  digitalWrite(PIN, ON);
 }
 
 // Repeatedly
