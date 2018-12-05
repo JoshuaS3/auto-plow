ffa80fdeba5ac1f5354bb10e6af092f5c7291a42
joshuas3 <joshstockin@gmail.com>
2018-12-05 19:35:30 +0000

    Input based events
    Creates an event-like callback function for Serial input.

--- src/ard1/serial.cpp
+++ src/ard1/serial.cpp
@@ -8,3 +8,25 @@ void serial(void) {
   while (!Serial)
     ;
 }
+
+int input_callback_count = 0;
+
+callback input_callbacks = (callback) malloc( sizeof( callback ) );
+
+void input(callback callback_function) {
+  input_callback_count ++;
+  callback input_callbacks_tmp = (callback) realloc(&input_callbacks, input_callback_count * sizeof( callback ));
+  if (!input_callbacks_tmp) {
+    exit(1);
+  }
+  input_callbacks = input_callbacks_tmp;
+}
+
+void input_update(void) {
+  if (Serial.available() > 0) {
+    String line = Serial.readStringUntil(';');
+    for (int x = 0; x < input_callback_count; x++) {
+      input_callbacks[x](line);
+    }
+  }
+}
--- src/ard1/serial.h
+++ src/ard1/serial.h
@@ -1,6 +1,9 @@
 #ifndef __SERIAL_H__
 #define __SERIAL_H__ 1
 
+typedef void (* callback)(String);
 void serial(void);
+void input(callback);
+void input_update(void);
 
 #endif
