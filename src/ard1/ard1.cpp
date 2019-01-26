b3fba95836eab955e2acf0513f12e9f77fb3ab50
joshuas3 <joshstockin@gmail.com>
2019-01-26 17:13:06 +0000

    Full control

--- src/ard1/ard1.cpp
+++ src/ard1/ard1.cpp
@@ -1,10 +1,14 @@
+// Copyright (c) Joshua 'joshuas3' Stockin 2019
+
+#define DEBUG 1
+
 #include "Arduino.h"
 #include "ard1.h"
 #include "serial.h"
 #include "reset.h"
 
 #include "SoftwareSerial.h"
-SoftwareSerial roboclaw(4, 5);
+SoftwareSerial roboclaw(ROBOCLAW_RX, ROBOCLAW_TX);
 
 // One time
 void setup() {
@@ -13,11 +17,22 @@ void setup() {
   serial();
 
   Serial.println("ARD1: ARD1 serial communications active");
+
+  pinMode(ROBOCLAW_LOGIC, OUTPUT);
+  digitalWrite(ROBOCLAW_LOGIC, HIGH);
+
   Serial.print("ARD1: Beginning RoboClaw Motor Controller serial at baud rate ");
   Serial.println(ROBOCLAW_BAUD);
   roboclaw.begin(ROBOCLAW_BAUD);
 
-  pinMode(PIN_BRAKES, OUTPUT);
+  pinMode(PIN_MOTOR_BRAKES, OUTPUT);
+  pinMode(PIN_LIGHTS, OUTPUT);
+
+  pinMode(PIN_PLOW_LIFT_UP, OUTPUT);
+  pinMode(PIN_PLOW_LIFT_DOWN, OUTPUT);
+  pinMode(PIN_PLOW_TILT_LEFT, OUTPUT);
+  pinMode(PIN_PLOW_TILT_RIGHT, OUTPUT);
+
   pinMode(PIN_CH_1, INPUT);
   pinMode(PIN_CH_2, INPUT);
   pinMode(PIN_CH_3, INPUT);
@@ -26,17 +41,22 @@ void setup() {
   pinMode(PIN_CH_6, INPUT);
 }
 
-int brakes = 1;
+// Calculate sensitivity threshold
+int controller_threshold = (5 - CONTROLLER_SENSITIVITY) * 10;
+int joystick_max = 64 + controller_threshold;
+int joystick_min = 64 - controller_threshold;
 
-byte full_stop = 0;
 // Repeatedly
 void loop() {
-  int ch1 = pulseIn(PIN_CH_1, 1000);
-  int ch2 = pulseIn(PIN_CH_2, 1000);
-  int ch3 = pulseIn(PIN_CH_3, 1000);
-  int ch4 = pulseIn(PIN_CH_4, 1000);
-  int ch5 = pulseIn(PIN_CH_5, 1000);
-  int ch6 = pulseIn(PIN_CH_6, 1000);
+  int start = millis();
+
+  // Read from radio receiver
+  int ch1 = pulseIn(PIN_CH_1, RADIO_TIMEOUT);
+  int ch2 = pulseIn(PIN_CH_2, RADIO_TIMEOUT);
+  int ch3 = pulseIn(PIN_CH_3, RADIO_TIMEOUT);
+  int ch4 = pulseIn(PIN_CH_4, RADIO_TIMEOUT);
+  int ch5 = pulseIn(PIN_CH_5, RADIO_TIMEOUT);
+  int ch6 = pulseIn(PIN_CH_6, RADIO_TIMEOUT);
 
   // ch1: right joystick horizontal, 1099, 1978
   // ch2: right joystick vertical,   1003, 1986
@@ -45,6 +65,7 @@ void loop() {
   // ch5: switch d,                  993,  1986
   // ch6: switch a,                  993,  1986
 
+  // Constrain radio information
   if (ch1 < 1099) ch1 = 1099;
   if (ch1 > 1978) ch1 = 1978;
 
@@ -63,43 +84,130 @@ void loop() {
   if (ch6 < 993)  ch6 = 993;
   if (ch6 > 1986) ch6 = 1986;
 
+  // Map receiver information
   int ch1_fixed = map(ch1, 1099, 1978, 1, 127);
   int ch2_fixed = map(ch2, 1003, 1986, 1, 127);
+  int ch3_fixed = map(ch3, 993, 1885, 1, 127);
+  int ch4_fixed = map(ch4, 1023, 1986, 1, 127);
+  int ch5_fixed = (ch5 > 1500); // 1 or 0
+  int ch6_fixed = (ch6 > 1500); // 1 or 0
 
-  if (ch1_fixed < 84 && ch1_fixed > 44) ch1_fixed = 64;
-  if (ch2_fixed < 84 && ch2_fixed > 44) ch2_fixed = 64;
+  // Calculate tilt direction
+  int tilt = 0;
+  if (ch4_fixed < joystick_min) tilt = -1;
+  if (ch4_fixed > joystick_max) tilt = 1;
+
+  // Calculate lift direction
+  int lift = 0;
+  if (ch3_fixed < joystick_min) lift = -1;
+  if (ch3_fixed > joystick_max) lift = 1;
+
+  // Calculate motor directions
+  if (ch1_fixed < joystick_max && ch1_fixed > joystick_min) ch1_fixed = 64;
+  if (ch2_fixed < joystick_max && ch2_fixed > joystick_min) ch2_fixed = 64;
 
   int motor_left = 64;
   int motor_right = 64;
 
-  if (ch1_fixed > 64 && ch2_fixed > 64) {
-    motor_left = ch2_fixed;
-    motor_right = 127 - (ch1_fixed - 64);
-  } else if (ch1_fixed < 64 && ch2_fixed > 64) {
-    motor_right = ch2_fixed;
-    motor_left = 127 - (64 - ch1_fixed);
-  } else if (ch2_fixed > 64) {
-    motor_right = ch2_fixed;
-    motor_left = ch2_fixed;
-  } else if (ch1_fixed > 64 && ch2_fixed < 64) {
-    motor_left = ch2_fixed;
-    motor_right = (64 - (64 - ch1_fixed)) - 64;
-  } else if (ch1_fixed < 64 && ch2_fixed < 64) {
-    motor_right = ch2_fixed;
-    motor_left = (64 - (ch1_fixed + 64)) + 64;
-  } else if (ch2_fixed < 64) {
-    motor_left = ch2_fixed;
-    motor_right = ch2_fixed;
-  } else if (ch1_fixed < 64) {
-    motor_right = (64 - ch1_fixed) + 64;
-    motor_left = (64 + ch1_fixed) - 64;
+  if (ch1_fixed < 64) {
+    motor_right = 64 + (64 - ch1_fixed);
+    motor_left = 64 - (64 - ch1_fixed);
+  } else if (ch1_fixed > 64) {
+    motor_left = 64 + (ch1_fixed - 64);
+    motor_right = 64 - (ch1_fixed - 64);
   }
+
+  if (ch2_fixed > 64) {
+    motor_right += (ch2_fixed - 64);
+    motor_left += (ch2_fixed - 64);
+  } else if (ch2_fixed < 64) {
+    motor_right -= (64 - ch2_fixed);
+    motor_left -= (64 - ch2_fixed);
+  }
+
+  // Constrain motor directions
+  if (motor_right > 127) motor_right = 127;
+  if (motor_right < 1) motor_right = 1;
+  if (motor_left > 127) motor_left = 127;
+  if (motor_left < 1) motor_left = 1;
+
+  // Offset motor_right number for RoboClaw serial format
+  motor_right += 128;
+
+  #ifdef DEBUG
+  Serial.println("\e[1;1H\e[2J");
+  Serial.println("Debug Information");
+  Serial.print("Left");
+  Serial.print("\t");
+  Serial.print("Right");
+  Serial.print("\t");
+  Serial.print("Lights");
+  Serial.print("\t");
+  Serial.print("Brakes + Motors");
+  Serial.print("\t\t");
+  Serial.print("Plow Tilt");
+  Serial.print("\t");
+  Serial.print("Plow Lift");
+  Serial.println();
 
   Serial.print(motor_left);
   Serial.print("\t");
-  Serial.println(motor_right);
+  Serial.print(motor_right);
+  Serial.print("\t");
+  Serial.print(ch6_fixed);
+  Serial.print("\t");
+  Serial.print(ch5_fixed);
+  Serial.print("\t\t\t");
+  Serial.print(tilt);
+  Serial.print("\t\t");
+  Serial.println(lift);
+  #endif
 
-//  roboclaw.write(127);
-//  roboclaw.write(255);
+  // Write motor information to RoboClaw
+  roboclaw.write((byte)motor_left);
+  roboclaw.write((byte)motor_right);
+
+  // Plow tilt
+  if (tilt == -1) {
+    digitalWrite(PIN_PLOW_TILT_RIGHT, LOW);
+    digitalWrite(PIN_PLOW_TILT_LEFT, HIGH);
+  } else if (tilt == 1) {
+    digitalWrite(PIN_PLOW_TILT_LEFT, LOW);
+    digitalWrite(PIN_PLOW_TILT_RIGHT, HIGH);
+  } else {
+    digitalWrite(PIN_PLOW_TILT_RIGHT, LOW);
+    digitalWrite(PIN_PLOW_TILT_LEFT, LOW);
+  }
+
+  // Plow lift
+  if (lift == -1) {
+    digitalWrite(PIN_PLOW_LIFT_UP, LOW);
+    digitalWrite(PIN_PLOW_LIFT_DOWN, HIGH);
+  } else if (tilt == 1) {
+    digitalWrite(PIN_PLOW_LIFT_DOWN, LOW);
+    digitalWrite(PIN_PLOW_LIFT_UP, HIGH);
+  } else {
+    digitalWrite(PIN_PLOW_LIFT_UP, LOW);
+    digitalWrite(PIN_PLOW_LIFT_DOWN, LOW);
+  }
+
+  // Lights
+  if (ch6_fixed) {
+    digitalWrite(PIN_LIGHTS, HIGH);
+  } else {
+    digitalWrite(PIN_LIGHTS, LOW);
+  }
+
+  // Power to motor controller
+  if (ch5_fixed) {
+    digitalWrite(PIN_MOTOR_BRAKES, HIGH);
+  } else {
+    digitalWrite(PIN_MOTOR_BRAKES, LOW);
+  }
+
+  int end = millis();
+  #ifdef DEBUG
+  Serial.print("Cycle time: ");
+  Serial.println(end - start);
+  #endif
 }
-
--- src/ard1/ard1.h
+++ src/ard1/ard1.h
@@ -1,9 +1,20 @@
+#include "Arduino.h"
+
 #ifndef __ARD1_H__
 #define __ARD1_H__ 1
 
-// Do not use pin 4, it's the dead pin for the RoboClaw SoftwareSerial
+// Pins
 
-#define PIN_BRAKES 7
+// 0 and 1 are hardware RX and TX
+#define PIN_RESET 2
+
+#define ROBOCLAW_LOGIC 3
+
+#define ROBOCLAW_RX 4
+#define ROBOCLAW_TX 5
+
+#define PIN_MOTOR_BRAKES 6
+#define PIN_LIGHTS 7
 
 #define PIN_CH_1 8
 #define PIN_CH_2 9
@@ -12,7 +23,16 @@
 #define PIN_CH_5 12
 #define PIN_CH_6 13
 
-#define PIN_RESET 2
+#define PIN_PLOW_LIFT_UP A0
+#define PIN_PLOW_LIFT_DOWN A1
+#define PIN_PLOW_TILT_LEFT A2
+#define PIN_PLOW_TILT_RIGHT A3
+
+// Other settings
+
+#define CONTROLLER_SENSITIVITY 4 // 1-5
+
+#define RADIO_TIMEOUT 1000
 
 #define BAUD_RATE 9600
 #define ROBOCLAW_BAUD 38400
